fa0bb6b9db0b7d93cd736ae37e8a57f0652a3dda
Hei Jing Tsang <tsangh@purdue.edu>
2019-04-10 06:51:55 -0400

    added some flask_login features

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import datetime
 from flask import Flask, render_template, redirect, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_talisman import Talisman
+from flask_login import current_user, login_user, login_manager, logout_user, login_required
 
 
 app = Flask(__name__)
@@ -9,7 +10,9 @@ Talisman(app)
 app.secret_key = "CS252 Spring 2019 Lab 6"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cs252sp19lab6.db'
 db = SQLAlchemy(app)
-app = Flask(__name__)
+login = login_manager(app)
+login.login_view = 'login'
+
 
 
 @app.route('/')
@@ -34,18 +37,30 @@ def report():
 
 @app.route('/admin', methods=['POST', 'GET'])
 def adminLogin():
+    if current_user.is_authenticated:
+        return redirect(url_for('adminQueue'))
+
+
     return render_template('admin_login.html')
 
 
 @app.route('/admin/reported', methods=['POST', 'GET'])
-def reported():
+@login_required
+def adminReported():
     return render_template('admin_reported.html')
 
 
-@app.route('admin/queue', methods=['POST', 'GET'])
-def queue():
+@app.route('/admin/queue', methods=['POST', 'GET'])
+@login_required
+def adminQueue():
     return render_template('admin_queue.html')
 
+
+@app.route('/admin/logout')
+@login_required
+def adminLogout():
+    logout_user()
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
