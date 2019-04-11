db675b02d2a17906bcaa23e0bfa2ec8ffc221b9e
Geonho Kim <kgh475926@gmail.com>
2019-04-10 20:58:33 -0400

    implement censorByWord function

--- testCensor.py
+++ testCensor.py
@@ -1,3 +1,26 @@
 #!/usr/bin/env python3
+# print ("Hello World!")
 
-print ("Hello World!")
+def censorByWord(text, word):
+    t = text.split()
+    n = []
+
+    for i in t:
+        if(i == word):
+            n.append("*" * len(word))
+        else:
+            n.append(i)
+
+    return " ".join(n)
+
+
+
+
+if __name__ == '__main__':
+    # print ("Hello World in main!")
+    text = input("Enter your text: ").lower()
+    word = "one" # need to be a list
+
+    censoredText = censor(text, word)
+
+    print ("CensoredText:" + censoredText)
