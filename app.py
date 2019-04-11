1cb70684c9534bdbcc8e467daf967b56a17e79c5
Hei Jing Tsang <tsangh@purdue.edu>
2019-04-11 19:06:37 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,196 @@
+import datetime, os
+from flask import Flask, render_template, redirect, flash, url_for, request, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+from flask_talisman import Talisman
+from flask_login import current_user, login_user, LoginManager, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_ckeditor import CKEditor, upload_success, upload_fail
+
+dirname = os.path.dirname(__file__)
+
+app = Flask(__name__)
+#Talisman(app)
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+ckeditor = CKEditor(app)
+app.secret_key = "CS252 Spring 2019 Lab 6"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cs252sp19lab6.db'
+app.config['CKEDITOR_SERVE_LOCAL'] = True
+app.config['CKEDITOR_HEIGHT'] = 400
+app.config['CKEDITOR_FILE_UPLOADER'] = 'upload'
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+
+class Admin(db.Model):
+    username = db.Column(db.String(64), nullable=False, primary_key=True, unique=True)
+    password = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+
+
+class Secrets(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    content = db.Column(db.Text, nullable=False)
+    submit_time = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow())
+    post_time = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow())
+
+    def __init__(self, content):
+        self.content = content
+
+
+class Queue(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    content = db.Column(db.Text, nullable=False)
+    submit_time = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow())
+
+    def __init__(self, content):
+        self.content = content
+
+
+class Reported(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    reason = db.Column(db.Text, nullable=False)
+    count = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, content, reason, count):
+        self.content = content
+        self.reason = reason
+        self.count = count
+
+
+db.create_all()
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/wall')
+def wall():
+    secrets = Secrets.query.order_by(Secrets.id.desc()).all()
+    return render_template('wall.html', secrets=secrets)
+
+
+@app.route('/add', methods=['POST', 'GET'])
+def add():
+    if request.method == 'POST':
+        if not request.form.get('ckeditor'):
+            flash('Post cannot be empty.', 'error')
+        else:
+            content = request.form.get('ckeditor')
+            '''
+            TODO: filtering part, now assume everything is clean.
+            '''
+            post = Secrets(content=content)
+            db.session.add(post)
+            db.session.commit()
+            return redirect(url_for('wall'))
+
+    return render_template('add.html')
+
+
+@app.route('/report', methods=['POST', 'GET'])
+def report():
+    if request.method == 'POST':
+        if not request.form['id'] or not request.form['reason']:
+            flash('Please enter all the fields', 'error')
+        else:
+            id = request.form['id']
+            secret = Secrets.query.filter_by(id=id).first()
+            reason = request.form['reason']
+            if not secret:
+                flash('Invalid ID.', 'error')
+            elif not Reported.query.filter_by(id=id).first():
+                report = Reported(id=id, reason=reason, count=1)
+                db.session.add(report)
+                db.session.commit()
+                flash('Report has been sent to the administrator.')
+                return redirect(url_for('wall'))
+            else:
+                report = Reported.query.filter_by(id=id).first()
+                data = report.data
+                data['count'] = data['count'] + 1
+                data['reason'] = data['reason'] + '\n' + request.form['reason']
+                report.data = data
+                db.session.merge(report)
+                db.session.flush()
+                db.session.commit()
+                flash('Report has been sent to the administrator.')
+                return redirect(url_for('wall'))
+
+    return render_template('report.html')
+
+
+@app.route('/admin', methods=['POST', 'GET'])
+def adminLogin():
+    if current_user.is_authenticated:
+        return redirect(url_for('adminQueue'))
+
+    if request.method == 'POST':
+        if not request.form['username'] or not request.form['password']:
+            flash('Please enter both your username and password.', 'error')
+        else:
+            user = Admin.get(request.form['username'])
+
+            if user.check_password(request.form['password']):
+                login_user(user)
+                return redirect(url_for('adminQueue'))
+            else:
+                flash('Invalid login.', 'error')
+
+    return render_template('admin_login.html')
+
+
+@app.route('/admin/reported', methods=['POST', 'GET'])
+@login_required
+def adminReported():
+    return render_template('admin_reported.html', report=Reported.query.all())
+
+
+@app.route('/admin/queue', methods=['POST', 'GET'])
+@login_required
+def adminQueue():
+    return render_template('admin_queue.html', queue=Queue.query.all())
+
+
+@app.route('/admin/logout')
+@login_required
+def adminLogout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route('/files/<path:filename>')
+def uploaded_files(filename):
+    path = dirname + '/bin/'
+    return send_from_directory(path, filename)
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    f = request.files.get('upload')
+    extension = f.filename.rsplit('.', 1)[1].lower()
+
+    if extension not in ['jpg', 'gif', 'png', 'jpeg']:
+        return upload_fail(message='Image only.')
+    bin_path = dirname + '/bin/'
+    f.save(os.path.join(bin_path, f.filename))
+    url = url_for('uploaded_files', filename=f.filename)
+
+    return upload_success(url=url)
+
+
+if __name__ == '__main__':
+    app.run()
